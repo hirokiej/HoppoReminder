3a74e671a793443dcf7ed15f9c88fbe38d90a03a
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-27 01:17:42 +0900

    今までエラーになっていたRuboCopの違反を修正した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include SessionsHelper
-  
+
   before_action :check_logged_in
 
   def check_logged_in
