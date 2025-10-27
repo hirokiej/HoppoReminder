6bfe0dd08cef46f037de41afa98df51171446589
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-28 01:23:45 +0900

    メールアドレスを固定の暗号文にした

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   encrypts :google_token
   encrypts :google_refresh_token
-  encrypts :email
+  encrypts :email, deterministic: true
   encrypts :line_channel_secret
   encrypts :line_channel_access_token
 
