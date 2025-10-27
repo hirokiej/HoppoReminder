70c7f3f20941d45df30ac469e359a54443cea77a
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-27 21:44:48 +0900

    LINE公式のトークンを取得する導線を作成

--- app/controllers/admin/line_infos_controller.rb
+++ app/controllers/admin/line_infos_controller.rb
@@ -1,7 +1,21 @@
 class Admin::LineInfosController < ApplicationController
   def edit
+    @admin = current_user
   end
 
   def update
+    @admin = current_user
+    if @admin.update(line_info_params)
+      redirect_to schedule_path, notice: 'LINE情報を更新しました'
+    else
+      log_out
+      redirect_to root_path, notice: 'もう一度やり直してください'
+    end
+  end
+
+  private
+
+  def line_info_params
+    params.require(:admin).permit(:line_channel_id, :line_channel_access_token, :line_channel_secret)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,19 @@ class SessionsController < ApplicationController
   skip_before_action :check_logged_in, only: :create
 
   def create
-    if (admin = Admin.find_or_create_from_auth_hash(auth_hash))
-      log_in admin
+    admin = Admin.find_or_create_from_auth_hash(auth_hash)
+
+    unless admin
+      redirect_to root_path, alert: 'ログインに失敗しました'
     end
-    redirect_to root_path
+
+    log_in admin
+
+    if admin.line_channel_id.blank?
+      redirect_to line_info_admin_path(admin)
+    else
+      reidirect_to schedule_path
+    end
   end
 
   def destroy
