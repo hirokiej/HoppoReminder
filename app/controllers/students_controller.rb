5c59667d161ce6064f2d7e921133eecba727168f
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-29 00:10:30 +0900

    /studentsでreal_nameを一括更新できるようにした

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,18 @@
 class StudentsController < ApplicationController
   def index
   end
+
+  def bulk_update
+    if current_user.update(user_params)
+      redirect_to students_path, notice: '名前を変更しました'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:admin).permit(students_attributes: [ :id, :real_name ])
+  end
 end
