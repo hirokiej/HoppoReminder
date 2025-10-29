9d94f89e2b6887b85d24924d5fe7dcf459e71bf2
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-29 23:34:24 +0900

    students/schedulesコントローラーを生成

---
+++ test/controllers/students/schedules_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class Students::SchedulesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get show' do
+    get students_schedules_show_url
+    assert_response :success
+  end
+end
