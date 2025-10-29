4ae7abd7480e607b4a96f1673137c54e84af11a0
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-29 23:10:17 +0900

    スケジュール変更ページを作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,12 +1,41 @@
 class SchedulesController < ApplicationController
-  before_action :set_schedules
+  before_action :set_schedule, only: %i[edit update]
 
   def index
     schedules = fetch_google_calendar_events(current_user)
     update_lesson_events(schedules)
   end
 
+  def edit;end
+
+  def update
+    if @schedule.update(schedule_params)
+      update_google_event(@schedule)
+
+      redirect_to schedules_path, notice: '日時を変更しました'
+    else
+      redirect_to schedules_path, notice: '変更できませんでした'
+    end
+  end
+
   private
+
+  def update_google_event(schedule)
+    service = google_calendar_service_for(current_user)
+    calendar_id = 'primary'
+
+    event_id = schedule.google_event_id
+    start_at = schedule.start_at.in_time_zone('Asia/Tokyo').to_datetime
+    end_at = start_at + 45.minutes
+
+    event = Google::Apis::CalendarV3::Event.new(
+      summary: @schedule.summary,
+      start: Google::Apis::CalendarV3::EventDateTime.new(date_time: start_at, time_zone: 'Asia/Tokyo'),
+      end:   Google::Apis::CalendarV3::EventDateTime.new(date_time: end_at, time_zone: 'Asia/Tokyo')
+    )
+
+    service.update_event(calendar_id, event_id, event)
+  end
 
   def update_lesson_events(events)
     events.each do |event|
@@ -23,7 +52,15 @@ class SchedulesController < ApplicationController
     end
   end
 
+  def schedule_params
+    params.require(:schedule).permit(:start_at)
+  end
+
   def set_schedules
     @schedules = Schedule.where(student: current_user.students)
+  end
+
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
   end
 end
