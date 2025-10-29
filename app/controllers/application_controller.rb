1ce25309339c899e961dc877bd1978cf2b4e2ebe
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-29 17:49:38 +0900

    Googleカレンダーのeventでreal_nameに一致するものをScheduleに登録できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,39 @@ class ApplicationController < ActionController::Base
 
     redirect_to root_path
   end
+
+  private
+
+  def fetch_google_calendar_events(admin)
+    calendar_service = google_calendar_service_for(admin)
+
+    calendar_id = 'primary'
+    response = calendar_service.list_events(
+      calendar_id,
+      max_results: 50,
+      single_events: true,
+      order_by: 'startTime',
+      time_min: Time.now.iso8601
+      )
+
+    response.items.map do |schedule|
+      {
+        google_event_id: schedule.id,
+        summary: schedule.summary,
+        start: (schedule.start.date || schedule.start.date_time).to_time
+      }
+    end
+  end
+
+  def google_calendar_service_for(admin)
+    calendar_service = Google::Apis::CalendarV3::CalendarService.new
+    credentials = Google::Auth::UserRefreshCredentials.new(
+      client_id: Rails.application.credentials.google[:client_id],
+      client_secret: Rails.application.credentials.google[:client_secret],
+      scope: [ 'https://www.googleapis.com/auth/calendar' ],
+      refresh_token: admin.google_refresh_token
+    )
+    calendar_service.authorization = credentials
+    calendar_service
+  end
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,29 @@
 class SchedulesController < ApplicationController
+  before_action :set_schedules
+
   def index
+    schedules = fetch_google_calendar_events(current_user)
+    update_lesson_events(schedules)
+  end
+
+  private
+
+  def update_lesson_events(events)
+    events.each do |event|
+      student = current_user.students.detect { |f| event[:summary].to_s.include?(f.real_name) }
+
+      if student
+        schedule = Schedule.find_or_initialize_by(google_event_id: event[:google_event_id])
+        schedule.update!(
+          summary: event[:summary],
+          start_at: event[:start],
+          student: student
+        )
+      end
+    end
+  end
+
+  def set_schedules
+    @schedules = Schedule.where(student: current_user.students)
   end
 end
