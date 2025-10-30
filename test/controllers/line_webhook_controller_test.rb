d81e8880c71a2c51540ab1ad32f44d5fd3726870
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-10-31 00:19:21 +0900

    Rspecを削除しMinitestに変更
    テストファイルのRubocopを通した

--- test/controllers/line_webhook_controller_test.rb
+++ test/controllers/line_webhook_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class LineWebhookControllerTest < ActionDispatch::IntegrationTest
   test 'should get callback' do
     get line_webhook_callback_url
+
     assert_response :success
   end
 end
