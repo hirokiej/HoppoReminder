23956851fb9b0043d0a9db2676e10d168131eb62
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-05 11:20:32 +0900

    ログイン中にログインページにアクセスした場合、/schedulesにリダイレクトするよう変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :check_logged_in, only: %i[index privacy term]
   def index
+    redirect_to schedules_path if current_user
   end
 
   def privacy; end
