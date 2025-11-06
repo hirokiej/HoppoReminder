ef21e5faf1e3fb9391ff793a8621bdf2fc17a779
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-07 00:44:24 +0900

    カレンダーの高さを変え不要なボタンを削除

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -5,12 +5,8 @@ export default class extends Controller {
   connect() {
     const calendar = new FullCalendar.Calendar(this.element, {
       initialView: 'dayGridMonth',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,listWeek'
-      },
-      events: '/calendars.json'
+      events: '/calendars.json',
+      height: 'auto'
     })
     calendar.render()
   }
