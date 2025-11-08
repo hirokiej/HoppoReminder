e9b3beeca25b6e58304c35c8698d3cb507906d38
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-08 17:48:57 +0900

    タブメニューの予定変更をレッスン変更に修正

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,7 +1,7 @@
 module NavigationHelper
   def navigation_tabs
     [
-      { name: '予定変更', path: schedules_path },
+      { name: 'レッスン変更', path: schedules_path },
       { name: 'カレンダー', path: calendars_path },
       { name: '生徒一覧', path: students_path }
     ]
