93b67d186643d7e65e3543a4b5584417c05cac48
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-08 17:13:00 +0900

    application_helperにメタタグとOGPの設定をした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,27 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'ほっぽリマインダー',
+      reverse: true,
+      charset: 'utf-8',
+      description: '',
+      keywords: 'ほっぽリマインダー, hopporeminder, ほっぽり, リマインダー, reminder, レッスン, LINE公式, Googleカレンダー',
+      og: {
+        title: :title,
+        type: 'website',
+        site_name: 'ほっぽリマインダー',
+        description: :description,
+        image: image_url('ogp.png'),
+        url: 'https://hopporeminder.com',
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@hirokiej'
+      }
+    }
+  end
+
   def format_japanese_datetime(datetime)
     datetime.strftime('%-m月%-d日%H時%M分')
   end
