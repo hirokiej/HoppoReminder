ea4d77100e3235975266b7070eca39ab74f35b06
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-08 17:55:14 +0900

    レッスン一覧表示を2ヶ月先(8件)までにした

--- app/controllers/students/schedules_controller.rb
+++ app/controllers/students/schedules_controller.rb
@@ -1,6 +1,7 @@
 class Students::SchedulesController < ApplicationController
+  TWO_MONTHS = 8
   def show
     @student = current_user.students.find(params[:id])
-    @schedules = @student.schedules.where('start_at >= ?', Time.current).order(:start_at)
+    @schedules = @student.schedules.where('start_at >= ?', Time.current).order(:start_at).limit(TWO_MONTHS)
   end
 end
