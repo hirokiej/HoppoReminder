82c4b22d597bf2938c5473b537285823626bb97a
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-10 17:48:25 +0900

    Rails8.1にアップデート

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_28_155141) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_10_081425) do
   create_table "admins", force: :cascade do |t|
-    t.string "name"
+    t.datetime "created_at", null: false
     t.string "email", null: false
+    t.string "google_refresh_token"
+    t.string "google_token"
+    t.string "line_channel_access_token"
     t.string "line_channel_id"
     t.string "line_channel_secret"
-    t.string "line_channel_access_token"
-    t.string "google_token"
-    t.string "google_refresh_token"
-    t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["line_channel_id"], name: "index_admins_on_line_channel_id", unique: true
   end
 
   create_table "schedules", force: :cascade do |t|
-    t.integer "student_id", null: false
-    t.string "summary", null: false
+    t.datetime "created_at", null: false
     t.string "google_event_id", null: false
     t.datetime "start_at", null: false
-    t.datetime "created_at", null: false
+    t.integer "student_id", null: false
+    t.string "summary", null: false
     t.datetime "updated_at", null: false
     t.index ["google_event_id"], name: "index_schedules_on_google_event_id", unique: true
     t.index ["student_id"], name: "index_schedules_on_student_id"
@@ -38,10 +38,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_28_155141) do
 
   create_table "students", force: :cascade do |t|
     t.integer "admin_id", null: false
-    t.string "line_user_id", null: false
-    t.string "line_display_name"
-    t.string "real_name"
     t.datetime "created_at", null: false
+    t.string "line_display_name"
+    t.string "line_user_id", null: false
+    t.string "real_name"
     t.datetime "updated_at", null: false
     t.index ["admin_id"], name: "index_students_on_admin_id"
     t.index ["line_user_id"], name: "index_students_on_line_user_id", unique: true
