213666f7bfeda8931395a5deebdeb2c3699da256
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-13 17:09:59 +0900

    schedulesでreal_nameが存在している場合のみ表示するようにした

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ApplicationRecord
   scope :with_real_name, -> { where.not(real_name: [ nil, '' ]) }
 
   def self.find_real_name(event, students)
-    students.detect do |student|
+    students.with_real_name.detect do |student|
       event[:summary].to_s.include?(student.real_name)
     end
   end
