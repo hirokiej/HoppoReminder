45367b879d9c78af9cb61da9c3c260b43ce01ee7
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-22 00:14:29 +0900

    adminsインテグレーションテストを追加

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = admins(:alice)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(@admin)
+  end
+
+  test 'should delete admin' do
+    delete admin_path(@admin)
+
+    assert_redirected_to root_path
+    assert_equal '退会しました', flash[:notice]
+    assert_nil Admin.find_by(id: '@admin.id')
+  end
 end
