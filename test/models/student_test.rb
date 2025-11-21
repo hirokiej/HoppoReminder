4deb9c92108b183cf6f2340f4bec08e010ebf1b8
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-21 15:40:35 +0900

    adminとstudentのモデルテストを追加

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should find real name' do
+    bob_event = { summary: schedules(:bob_first_schedule).summary }
+    charlie_event = { summary: schedules(:charlie_first_schedule).summary }
+
+    students = Student.all
+
+    bob = Student.find_real_name(bob_event, students)
+    charlie = Student.find_real_name(charlie_event, students)
+
+    assert_equal 'ボブ', bob.real_name
+    assert_equal 'チャーリー', charlie.real_name
+  end
 end
