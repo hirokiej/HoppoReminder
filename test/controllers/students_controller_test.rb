56d1ab6346595f1b18358727484c67d71b33bf1b
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-21 23:50:48 +0900

    studentsインテグレーションテストを追加

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,9 +1,31 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get students_index_url
+  setup do
+    @admin = admins(:alice)
+    @bob = students(:student_bob)
+    @charlie = students(:student_charlie)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(@admin)
+  end
+
+  test 'should update real names in bulk' do
+    get students_path
 
     assert_response :success
+
+    patch bulk_update_students_path, params: {
+      admin: {
+        students_attributes: {
+          '0' => { id: @bob.id, real_name: '太郎' },
+          '1' => { id: @charlie.id, real_name: '次郎' }
+        }
+      }
+    }
+    @bob.reload
+    @charlie.reload
+
+    assert_equal '太郎', @bob.real_name
+    assert_equal '次郎', @charlie.real_name
   end
 end
