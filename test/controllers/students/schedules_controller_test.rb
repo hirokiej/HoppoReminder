1aea58168be13d559699f6c9d366b22dc28f5d8f
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-21 23:48:17 +0900

    不要なテストを削除

--- test/controllers/students/schedules_controller_test.rb
+++ test/controllers/students/schedules_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class Students::SchedulesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get show' do
-    get students_schedules_show_url
-
-    assert_response :success
-  end
 end
