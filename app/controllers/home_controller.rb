b164b57efc342c369f970ff649e4296e1e8b9be9
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-11-23 00:12:04 +0900

    文字列展開するためシングルからダブルクォートに修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :check_logged_in, only: %i[index privacy term]
   def index
-    redirect_to schedules_path, notice: '#{current_user.name}でログインしました' if current_user
+    redirect_to schedules_path, notice: "#{current_user.name}でログインしました" if current_user
   end
 
   def privacy; end
