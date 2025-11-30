fdf91667781f62e768640d40ccee420b4f0de2f0
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-01 06:43:15 +0900

    Adminのseedsとfixtureから不要なgoogleとLINEのカラムを削除

--- db/seeds/admins.rb
+++ db/seeds/admins.rb
@@ -3,9 +3,4 @@ Admin.destroy_all
 admin = Admin.create!(
   name: "ピアノの先生",
   email: "admin@example.com",
-  google_token: "google_token",
-  google_refresh_token: "google_refresh_token",
-  line_channel_id: "line_channel_id",
-  line_channel_secret: "line_channel_secret",
-  line_channel_access_token: "line_channel_access_token"
 )
