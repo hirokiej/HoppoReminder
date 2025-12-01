587726b75d6013c605de29debec2594cb0af3635
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-01 22:51:12 +0900

    lesson_time_notifications_jobのテストを書いた

--- test/jobs/lesson_time_notifications_job_test.rb
+++ test/jobs/lesson_time_notifications_job_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class LessonTimeNotificationsJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @schedule = schedules(:bob_first_schedule)
+    @student = students(:student_bob)
+    @job = LessonTimeNotificationsJob.new
+  end
+
+  test 'job is enqueued' do
+    assert_enqueued_with(job: LessonTimeNotificationsJob) do
+      LessonTimeNotificationsJob.perform_later(@schedule.id, @student.id, :before_lesson)
+    end
+  end
+
+  test 'message_templates return correct messages' do
+    messages = @job.send(:message_templates, @schedule)
+
+    assert_equal "レッスン時間が変更になりました。#{@schedule.start_at.strftime('%-m月%-d日%H時%M分')}です。", messages[:notice_now][:text]
+    assert_equal "レッスンは明日、#{@schedule.start_at.strftime('%-m月%-d日%H時%M分')}からあります。", messages[:before_lesson][:text]
+    assert_equal '明日はお休みです。', messages[:no_lesson_tomorrow][:text]
+  end
 end
