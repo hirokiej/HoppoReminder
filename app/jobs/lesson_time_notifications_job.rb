189109a8fb3b6a399bb22c695b66045e81a8d505
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-01 22:50:09 +0900

    message_templatesをprivateでメソッドとして定義した

--- app/jobs/lesson_time_notifications_job.rb
+++ app/jobs/lesson_time_notifications_job.rb
@@ -1,7 +1,7 @@
 class LessonTimeNotificationsJob < ApplicationJob
   queue_as :default
 
-  def perform(schedule_id, student_id, messages)
+  def perform(schedule_id, student_id, timing)
     schedule = Schedule.find(schedule_id)
     student = Student.find(student_id)
 
@@ -9,7 +9,14 @@ class LessonTimeNotificationsJob < ApplicationJob
       channel_access_token: student.admin.line_channel_access_token
     )
 
-    message_templates = {
+    message = message_templates(schedule)[timing.to_sym]
+    client.push_message(push_message_request: { to: student.line_user_id, timing: [ message ] })
+  end
+
+  private
+
+  def message_templates(schedule)
+    {
       notice_now: {
         type: 'text',
         text: "レッスン時間が変更になりました。#{schedule.start_at.strftime('%-m月%-d日%H時%M分')}です。"
@@ -25,8 +32,5 @@ class LessonTimeNotificationsJob < ApplicationJob
         text: '明日はお休みです。'
       }
     }
-
-    message = message_templates[messages.to_sym]
-    client.push_message(push_message_request: { to: student.line_user_id, messages: [ message ] })
   end
 end
