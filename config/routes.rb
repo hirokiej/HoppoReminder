82fd32553f1e9f3186068c3215a5d42a541cb2a0
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-01 23:39:16 +0900

    routes.rbで使用していないルーティングを制限

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   root 'home#index'
-  resources :admins do
+
+  resources :admins, only: %i[destroy] do
     member do
       get 'line_info', to: 'admin/line_infos#edit'
       patch 'line_info', to: 'admin/line_infos#update'
     end
   end
-  resources :students do
+
+  resources :students, only: %i[index] do
     collection do
       patch :bulk_update
     end
@@ -29,18 +21,16 @@ Rails.application.routes.draw do
 
   resources :schedules, only: %i[index edit update]
 
+  resources :sessions, only: %i[create destroy]
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get 'log_out', to: 'sessions#destroy', as: 'log_out'
-  post '/line_webhook/callback', to: 'line_webhook#callback'
 
-  resources :sessions, only: %i[create destroy]
-  resources :students, only: :index
+  post '/line_webhook/callback', to: 'line_webhook#callback'
 
   get 'privacy', to: 'home#privacy'
   get 'term', to: 'home#term'
 
-  if Rails.env.development?
-    get 'mock_login', to: 'application#mock_login'
-  end
+  get 'mock_login', to: 'application#mock_login' if Rails.env.development?
 end
