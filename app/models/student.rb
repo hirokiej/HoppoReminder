ce80db84ffca150776103bec8b3082943cc96656
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-03 15:10:58 +0900

    生徒のreal_nameにvalidatesを追加した(uniqueness,presence)

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,10 +2,10 @@ class Student < ApplicationRecord
   belongs_to :admin
   has_many :schedules, dependent: :destroy
 
-  scope :with_real_name, -> { where.not(real_name: [ nil, '' ]) }
+  validates :real_name, presence: true, uniqueness: true
 
   def self.find_real_name(event, students)
-    students.with_real_name.detect do |student|
+    students.detect do |student|
       event[:summary].to_s.include?(student.real_name)
     end
   end
