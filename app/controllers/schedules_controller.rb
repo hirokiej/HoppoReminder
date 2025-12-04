6abb3339b08b8ece2879f8e3ef506b975a56db9c
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-05 00:06:28 +0900

    カレンダーやレッスンの取得や変更のロジックをモデルに移行

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,25 +1,13 @@
 class SchedulesController < ApplicationController
   include Authenticatable
 
-  MOCK_ADMIN = 'ピアノの先生'
-  MOCK_CALENDAR_EVENTS = [
-    { google_event_id: '1', summary: 'テストレッスン１', start: Time.current + 3.days + 15.hours },
-    { google_event_id: '2', summary: 'テストレッスン2', start: Time.current + 10.days + 15.hours }
-  ]
-  GOOGLE_CALENDAR_MAX_RESULTS = 50
-  GOOGLE_CALENDAR_LOOKBACK = 1.year.ago
-
-  NOTICE_TIME_BEFORE_LESSON = 1.day
-  NOTICE_TIME_NO_LESSON_TOMORROW = 1.day
-  NOTICE_HOUR = 20
-
   before_action :set_schedules, only: :index
   before_action :set_schedule, only: %i[edit update]
 
   def index
-    schedules = fetch_google_calendar_events(current_user)
+    schedules = Schedule.fetch_google_calendar_events(current_user)
     upcoming_schedules = schedules.select { |schedule| schedule[:start] >= Time.current }
-    update_lesson_events(upcoming_schedules)
+    Schedule.update_lesson_events(upcoming_schedules, current_user)
 
     today = Date.current
     @schedules = @schedules.where('start_at >= ?', Time.current)
@@ -30,8 +18,8 @@ class SchedulesController < ApplicationController
   def update
     first_start_at = @schedule.start_at
     if @schedule.update(schedule_params)
-      update_google_event(@schedule)
-      notification_message(first_start_at)
+      @schedule.update_google_event(current_user)
+      @schedule.notification_message(first_start_at)
 
       redirect_to schedules_path, notice: 'カレンダーの日時を変更し、LINEの投稿予約も完了しました'
     else
@@ -40,74 +28,6 @@ class SchedulesController < ApplicationController
   end
 
   private
-
-  def update_google_event(schedule)
-    service = google_calendar_service_for(current_user)
-    calendar_id = 'primary'
-
-    event_id = schedule.google_event_id
-    start_at = schedule.start_at.in_time_zone('Asia/Tokyo').to_datetime
-    end_at = start_at + 45.minutes
-
-    event = Google::Apis::CalendarV3::Event.new(
-      summary: @schedule.summary,
-      start: Google::Apis::CalendarV3::EventDateTime.new(date_time: start_at, time_zone: 'Asia/Tokyo'),
-      end:   Google::Apis::CalendarV3::EventDateTime.new(date_time: end_at, time_zone: 'Asia/Tokyo')
-    )
-
-    service.update_event(calendar_id, event_id, event)
-  end
-
-  def update_lesson_events(events)
-    events.each do |event|
-      student = Student.identify_from_summary(event, current_user.students)
-
-      if student
-        schedule = Schedule.find_or_initialize_by(google_event_id: event[:google_event_id])
-        schedule.update!(
-          summary: event[:summary],
-          start_at: event[:start],
-          student: student
-        )
-      end
-    end
-  end
-
-  def fetch_google_calendar_events(admin)
-    if admin.name == MOCK_ADMIN
-      return MOCK_CALENDAR_EVENTS
-    end
-
-    calendar_service = google_calendar_service_for(admin)
-
-    response = calendar_service.list_events(
-      'primary',
-      max_results: GOOGLE_CALENDAR_MAX_RESULTS,
-      single_events: true,
-      order_by: 'startTime',
-      time_min: GOOGLE_CALENDAR_LOOKBACK.iso8601
-      )
-
-    response.items.map do |schedule|
-      {
-        google_event_id: schedule.id,
-        summary: schedule.summary,
-        start: (schedule.start.date || schedule.start.date_time).to_time
-      }
-    end
-  end
-
-  def google_calendar_service_for(admin)
-    calendar_service = Google::Apis::CalendarV3::CalendarService.new
-    credentials = Google::Auth::UserRefreshCredentials.new(
-      client_id: Rails.application.credentials.google[:client_id],
-      client_secret: Rails.application.credentials.google[:client_secret],
-      scope: [ 'https://www.googleapis.com/auth/calendar' ],
-      refresh_token: admin.google_refresh_token
-    )
-    calendar_service.authorization = credentials
-    calendar_service
-  end
 
   def schedule_params
     params.require(:schedule).permit(:start_at)
@@ -119,24 +39,5 @@ class SchedulesController < ApplicationController
 
   def set_schedule
     @schedule = Schedule.find(params[:id])
-  end
-
-  def notification_message(first_start_at)
-    notification_time = @schedule.start_at
-    LessonTimeNotificationsJob.perform_now(@schedule.id, @schedule.student.id, :notice_now)
-    enqueue_notification(
-      (notification_time - NOTICE_TIME_BEFORE_LESSON).change(hour: NOTICE_HOUR, min: 0),
-      :before_lesson
-    )
-    enqueue_notification(
-      (first_start_at - NOTICE_TIME_NO_LESSON_TOMORROW).change(hour: NOTICE_HOUR, min: 0),
-      :no_lesson_tomorrow
-    )
-  end
-
-  def enqueue_notification(wait_until, timing)
-    LessonTimeNotificationsJob
-    .set(wait_until: wait_until)
-    .perform_later(@schedule.id, @schedule.student.id, timing)
   end
 end
