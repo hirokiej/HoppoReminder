acc9983ce37073415991d2439b3622c20a949a00
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-06 03:32:14 +0900

    seedのAdminにlineに関するダミー情報を追加

--- db/seeds/admins.rb
+++ db/seeds/admins.rb
@@ -3,4 +3,7 @@ Admin.destroy_all
 admin = Admin.create!(
   name: "ピアノの先生",
   email: "admin@example.com",
+  line_channel_access_token: "123456789",
+  line_channel_id: "123456789",
+  line_channel_secret: "123456789",
 )
