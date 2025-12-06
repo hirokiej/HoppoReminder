f8de4a3f9126cbc187fbca84dfc8771cdd9ecaa7
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-07 02:12:21 +0900

    students/schedules_controllerのテストを書いた

--- test/controllers/students/schedules_controller_test.rb
+++ test/controllers/students/schedules_controller_test.rb
@@ -1,4 +1,16 @@
 require 'test_helper'
 
 class Students::SchedulesControllerTest < ActionDispatch::IntegrationTest
+  test 'should show right student lessons for total 8' do
+    admin = admins(:alice)
+    bob = students(:student_bob)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    get schedules_student_path(bob)
+
+    assert_response :success
+
+    assert_select 'li', text: /変更/, count: 8
+  end
 end
