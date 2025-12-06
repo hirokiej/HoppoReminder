5f2cb3c92281a86f7f858d2e0dc3f5442b507e27
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-07 03:40:28 +0900

    scheduleのモデルテストを追加

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
+require 'minitest/mock'
 
 class ScheduleTest < ActiveSupport::TestCase
+  include ActiveJob::TestHelper
+
   setup do
     @student = students(:student_bob)
+    @schedules = @student.schedules
   end
+
   test 'should start_today' do
     yesterday = Schedule.create!(
       student: @student,
@@ -23,5 +28,33 @@ class ScheduleTest < ActiveSupport::TestCase
 
     assert_includes upcoming_schedules, tomorrow
     refute_includes upcoming_schedules, yesterday
+  end
+
+  test 'should dispaly limited upcoming lessons' do
+    limited_upcoming_schedules = @schedules.limited_upcoming_lessons
+
+    assert_equal 8, limited_upcoming_schedules.count
+  end
+
+  test 'should update google event' do
+    lesson = schedules(:bob_first_schedule)
+
+    google_mock = Minitest::Mock.new
+    google_mock.expect(:update_event, true, [ 'primary', lesson.google_event_id, Google::Apis::CalendarV3::Event ])
+
+    Schedule.stub(:google_calendar_service_for, google_mock) do
+      lesson.update_google_event(lesson.student.admin)
+    end
+
+    google_mock.verify
+  end
+
+  test 'should notificate messages' do
+    schedule = schedules(:bob_first_schedule)
+    first_start_at = schedule.start_at + 3.days
+
+    schedule.notification_message(first_start_at)
+
+    assert_enqueued_jobs 2
   end
 end
