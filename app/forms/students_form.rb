c92ae3f35b9fab6e2c8c3af582ab4c12fa38ffea
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-08 19:23:27 +0900

    変更のあった生徒名だけ保存するようにした

--- app/forms/students_form.rb
+++ app/forms/students_form.rb
@@ -10,7 +10,8 @@ class StudentsForm
   def update(params)
     params.each do |student_param|
       student = students.find { |s| s.id == student_param[:id].to_i }
-      student.update(real_name: student_param[:real_name]) if student
+      student.real_name = student_param[:real_name]
+      student.update(real_name: student_param[:real_name]) if student.changed?
     end
   end
 end
