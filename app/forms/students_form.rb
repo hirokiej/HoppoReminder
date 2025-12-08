ba331262dd65840d1b965f7f548aff0d7e6a6f90
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-08 19:24:43 +0900

    生徒名の変更がない場合はエラーになるよう変更

--- app/forms/students_form.rb
+++ app/forms/students_form.rb
@@ -1,17 +1,22 @@
 class StudentsForm
   include ActiveModel::Model
 
-  attr_accessor :students
-
   def initialize(students)
     @students = students
   end
 
   def update(params)
+    any_changes = false
+
     params.each do |student_param|
-      student = students.find { |s| s.id == student_param[:id].to_i }
+      student = @students.find { |s| s.id == student_param[:id].to_i }
       student.real_name = student_param[:real_name]
-      student.update(real_name: student_param[:real_name]) if student.changed?
+
+      if student.changed?
+        student.save
+        any_changes = true
+      end
     end
+    any_changes
   end
 end
