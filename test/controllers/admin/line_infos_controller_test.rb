8df193bd47caa3739f96b8c50610e8c90f72df6a
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-12-09 02:32:38 +0900

    line_infos_controllerのテストを書いた

--- test/controllers/admin/line_infos_controller_test.rb
+++ test/controllers/admin/line_infos_controller_test.rb
@@ -1,4 +1,31 @@
 require 'test_helper'
 
 class Admin::LineInfosControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin = admins(:alice)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(@admin)
+  end
+
+  test 'should get line_info edit page' do
+    get line_info_admin_path(@admin)
+
+    assert_response :success
+  end
+
+  test 'should update line_infos' do
+    patch line_info_admin_path(@admin), params: {
+      admin: { line_channel_id: 123456789, line_channel_secret: 123456789, line_channel_access_token: 123456789 }
+    }
+
+    assert_redirected_to schedules_path
+  end
+
+  test 'should not update line_infos' do
+    patch line_info_admin_path(@admin), params: {
+      admin: { line_channel_id: 123456789, line_channel_secret: '', line_channel_access_token: '' }
+    }
+
+    assert_redirected_to line_info_admin_path(@admin)
+  end
 end
