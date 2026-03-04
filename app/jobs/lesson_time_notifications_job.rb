3be1b3a4ea994a9161a010fb2c7398d5dad514a1
hirokiej <123009866+hirokiej@users.noreply.github.com>
2026-03-05 01:14:30 +0900

    レッスン変更直後の文言に変更前の日時を追加し比較できるよう変更

--- app/jobs/lesson_time_notifications_job.rb
+++ app/jobs/lesson_time_notifications_job.rb
@@ -1,7 +1,7 @@
 class LessonTimeNotificationsJob < ApplicationJob
   queue_as :default
 
-  def perform(schedule_id, student_id, timing)
+  def perform(schedule_id, student_id, timing, first_start_at)
     schedule = Schedule.find(schedule_id)
     student = Student.find(student_id)
 
@@ -9,19 +9,20 @@ class LessonTimeNotificationsJob < ApplicationJob
       channel_access_token: student.admin.line_channel_access_token
     )
 
-    message = message_templates(schedule)[timing.to_sym]
+    message = message_templates(schedule, first_start_at)[timing.to_sym]
     client.push_message(push_message_request: { to: student.line_user_id, messages: [ message ] })
   end
 
   private
 
-  def message_templates(schedule)
-    start_time = I18n.l(schedule.start_at, format: :with_weekday)
+  def message_templates(schedule, first_start_at)
+    start_time = format_time(schedule.start_at)
+    old_start_time = format_time(first_start_at)
 
     {
       notice_now: {
         type: 'text',
-        text: "レッスンの日程が変更になりました。次回は#{start_time}からです。"
+        text: "レッスンの日程が変更になりました。\n変更前：#{old_start_time}\n変更後：#{start_time}"
       },
 
       before_lesson: {
@@ -34,5 +35,9 @@ class LessonTimeNotificationsJob < ApplicationJob
         text: '明日はお休みです。'
       }
     }
+  end
+
+  def format_time(time)
+    I18n.l(time, format: :with_weekday)
   end
 end
