6f20946a5604140fc2bde0ad67dba9f647582aa4
hirokiej <123009866+hirokiej@users.noreply.github.com>
2026-03-05 01:15:00 +0900

    message_templatesのテストが不要なので削除

--- test/jobs/lesson_time_notifications_job_test.rb
+++ test/jobs/lesson_time_notifications_job_test.rb
@@ -12,14 +12,4 @@ class LessonTimeNotificationsJobTest < ActiveJob::TestCase
       LessonTimeNotificationsJob.perform_later(@schedule.id, @student.id, :before_lesson)
     end
   end
-
-  test 'message_templates return correct messages' do
-    messages = @job.send(:message_templates, @schedule)
-
-    start_time = I18n.l(@schedule.start_at, format: :with_weekday)
-
-    assert_equal "レッスン時間が変更になりました。#{start_time}です。", messages[:notice_now][:text]
-    assert_equal "レッスンは明日、#{start_time}からあります。", messages[:before_lesson][:text]
-    assert_equal '明日はお休みです。', messages[:no_lesson_tomorrow][:text]
-  end
 end
